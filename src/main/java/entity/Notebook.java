878458c3789efea1ff25add518d0a4e6a5d9ab5a
AlexG <alexandru.st.gheorghiu@gmail.com>
2022-01-19 18:27:42 +0200

    Good one

--- src/main/java/entity/Notebook.java
+++ src/main/java/entity/Notebook.java
@@ -1,2 +1,28 @@
-package entity;public class Notebook {
+package entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "Notebook")
+public class Notebook extends Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "Notebook_ID")
+    private long id;
+
+    @Column(name = "Notebook_Format")
+    private String type;
+
+    @Column(name = "For_Which_Course")
+    private String forWhichCourse;
+
 }
