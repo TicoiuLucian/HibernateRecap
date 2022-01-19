6bef185ab12ed7ae123892ea581bdc018acacf93
AlexG <alexandru.st.gheorghiu@gmail.com>
2022-01-19 18:32:15 +0200

    add Pen

--- src/main/java/entity/Pen.java
+++ src/main/java/entity/Pen.java
@@ -1,4 +1,26 @@
 package entity;
 
-public class Pen {
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="Pen")
+public class Pen extends Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Pen_ID")
+    private long id;
+
+    @Column(name = "Color_Type")
+    private String color;
+
+    @Column(name = "Type_Pen")
+    private String type;
+
 }
