7fb9391b955218bddb756e8d839864ab465f45ba
AlexG <alexandru.st.gheorghiu@gmail.com>
2022-01-19 15:52:06 +0200

    added two entities and used inheritance

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import com.github.javafaker.Faker;
+import entity.Notebook;
+import entity.Pen;
 import entity.Student;
 import entity.Teacher;
 import org.hibernate.Session;
@@ -28,17 +30,46 @@ public class App {
 //        transaction.commit();
 //        session.save(address);
 
+        //Student student = new Student();
+        //student.setFirstName(faker.name().firstName());
+       // student.setLastName(faker.name().firstName());
+
+       // Teacher teacher = new Teacher();
+       // teacher.setFirstName(faker.name().firstName());
+       // teacher.setLastName(faker.name().lastName());
+      //  teacher.setCourse("Math");
+       // teacher.addStudentToTeacher(student);
+
+       // session.save(teacher);
         Student student = new Student();
         student.setFirstName(faker.name().firstName());
-        student.setLastName(faker.name().firstName());
+        student.setLastName(faker.name().lastName());
+        student.setCourse("Math");
 
         Teacher teacher = new Teacher();
         teacher.setFirstName(faker.name().firstName());
         teacher.setLastName(faker.name().lastName());
-        teacher.setCourse("Math");
-        teacher.addStudentToTeacher(student);
+        teacher.setCourse("History");
 
+
+
+        Pen pen = new Pen();
+        pen.setColor("Blue");
+        pen.setType("fontain pen");
+
+        Notebook notebook = new Notebook();
+        notebook.setType("A4");
+        notebook.setForWhichCourse("Math");
+
+        Student student1 = new Student();
+        student1.setFirstName(faker.name().firstName());
+        student1.setLastName(faker.name().lastName());
+
+        session.save(student);
         session.save(teacher);
+        session.save(notebook);
+        session.save(pen);
+
         transaction.commit();
 
         session.close();
